70253b3b169057814025209b884487b3816df3b9
Jayant <jayant_jeswar@outlook.com>
2021-07-29 13:55:48 +0530

    updated test from regular expression to literal string

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByRole('link', {name : /Learn v React/i});
+  const linkElement = screen.getByRole('link', {name : /Learn React/i});
   expect(linkElement).toBeInTheDocument();
 });
